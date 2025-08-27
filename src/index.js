7e6ab2ae1b6e1a8dc038c7bfe782cfc8e628ba0e
Jaroslav Makeichyk <jaroslau1@gmail.com>
2025-08-27 11:07:44 +0200

    feat: complete human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(n) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  // 21, 31 , 41, 51 --> 0 1 2 3
+  const tens = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const arr = n.toString().split('');
+  let res = '';
+
+  if (n < 20) {
+    res = numbers[n];
+    return res;
+  }
+
+  if (n < 100) {
+    res += tens[+arr[0] - 2];
+    res += ' ';
+    res += +arr[1] !== 0 ? numbers[+arr[1]] : '';
+    return res.trim();
+  }
+
+  if (n >= 100) {
+    res += `${numbers[+arr[0]]} hundred ${n % 100 === 0 ? '' : toReadable(n % 100)}`;
+    return res.trim();
+  }
+
+  return n;
 };
